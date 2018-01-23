251896e554c15d29894635fcd7816ac10daf1fc5
李田 <apple@AppledeMacBook-Air.local>
2018-01-23 19:45:39 +0800

    取消报事

--- src/router/index.js
+++ src/router/index.js
@@ -4,34 +4,38 @@ import HelloWorld from '@/components/HelloWorld'
 import maintain from '@/pages/maintain'
 import test1 from '@/pages/test1'
 import test2 from '@/pages/test2'
+import cancelReport from '@/pages/cancelReport'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'maintain',
-      component: maintain,
-      meta: {
-        title: '报事报修',
-      }
-    },
-    {
-      path: '/test1',
-      name: 'test1',
-      component: test1,
-      meta: {
-        title: '测试列表1',
-      }
-    },
-    {
-      path: '/test2',
-      name: 'test2',
-      component: test2,
-      meta: {
-        title: '测试列表2',
-      }
+  routes: [{
+    path: '/',
+    name: 'maintain',
+    component: maintain,
+    meta: {
+      title: '报事报修',
     }
-  ]
+  }, {
+    path: '/test1',
+    name: 'test1',
+    component: test1,
+    meta: {
+      title: '测试列表1',
+    }
+  }, {
+    path: '/test2',
+    name: 'test2',
+    component: test2,
+    meta: {
+      title: '测试列表2',
+    }
+  }, {
+    path: '/cancelReport',
+    name: 'cancelReport',
+    component: cancelReport,
+    meta: {
+      title: '取消报事',
+    }
+  }]
 })
